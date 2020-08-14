78c3542c3581c1673d0c2c10320cef3ebaeffdac
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-13 17:18:47 -0700

    Creating react app and producing basis for Table and TableRow components

---
+++ src/components/Table/index.js
@@ -0,0 +1,28 @@
+export function Table(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Number</th>
+                    <th>Role</th>
+                    <th>Position</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.children}
+            </tbody>
+        </table>
+        
+    )
+}
+
+export function TableRow() {
+    return (
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    )
+}
