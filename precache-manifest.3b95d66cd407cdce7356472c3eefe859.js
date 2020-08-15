65815f1a8df7cac052298bcce7b02b2faa012a4e
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-14 17:11:55 -0700

    Updates

---
+++ precache-manifest.3b95d66cd407cdce7356472c3eefe859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47bbe609c471d72d99d386eb2ef174d",
+    "url": "/TeamRoster/index.html"
+  },
+  {
+    "revision": "d1937c9f515530e0940b",
+    "url": "/TeamRoster/static/css/main.edb98739.chunk.css"
+  },
+  {
+    "revision": "9f0a7181dfc3cd827d1f",
+    "url": "/TeamRoster/static/js/2.476149ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TeamRoster/static/js/2.476149ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1937c9f515530e0940b",
+    "url": "/TeamRoster/static/js/main.1183ff0e.chunk.js"
+  },
+  {
+    "revision": "5d9d49cb3296f072813b",
+    "url": "/TeamRoster/static/js/runtime-main.cb5e527c.js"
+  }
+]);
